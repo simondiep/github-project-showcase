0f61f8b54830133cfeaf6161049b40ed38a44042
Simon Diep <simondiep1@gmail.com>
2018-09-24 12:03:30 -0500

    Added loading spinner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import RepoCard from "./components/RepoCard";
 import { GITHUB_USERNAME, REPOS_URL } from "./Constants";
+import Loading from "./loading-on-white.gif";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      hasLoadedRepositories: false,
       repositories: [],
     };
   }
@@ -17,11 +19,18 @@ class App extends Component {
     fetch(REPOS_URL)
       .then(response => response.json())
       .then(responseAsJson => {
-        this.setState({ repositories: responseAsJson.repos });
+        this.setState({ hasLoadedRepositories: true, repositories: responseAsJson.repos });
       });
   }
 
   render() {
+    if (!this.state.hasLoadedRepositories) {
+      return (
+        <div className="Loading">
+          <img src={Loading} alt="Loading" />
+        </div>
+      );
+    }
     return (
       <div className="App">
         <header className="App-header">
